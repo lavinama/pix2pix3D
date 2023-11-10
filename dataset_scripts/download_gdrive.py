22fd5d9b7dc859fdcc6e3a067eb523cd24a00754
mario-finesse <mario@finesse.com>
2023-11-10 21:03:20 +0000

    Added Deep3DFaceRecon_pytorch submodule

---
+++ dataset_scripts/download_gdrive.py
@@ -0,0 +1,7 @@
+from storage_utils import GoogleDriveAPI
+
+gdrive = GoogleDriveAPI()
+
+# gdrive.download_folder_from_url("https://drive.google.com/drive/folders/1mC6i4YmdpazJSmXrW8WFSfsImAJ8a_CF?usp=sharing", "data/")
+# gdrive.download_folder_from_url("https://drive.google.com/drive/folders/1yjTTE57P9-hEe-IVcE-GXdh04WGo5lD9?usp=sharing", "data/")
+gdrive.download_folder_from_url("https://drive.google.com/drive/folders/1XTPuu784DIvk0ie094qyLrcF-v-jMe3_?usp=sharing", "data/")
